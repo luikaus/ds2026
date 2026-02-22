b87a4e28eaff8a708dc27e17ec7508cd6c71961d
Eemeli Kyröläinen <eemeli.ky@gmail.com>
2026-02-22 02:27:47 +0200

    Added postgres data to store data about uploaded videos + made some changes to video transcoding and dockerfiles

--- core/app.py
+++ core/app.py
@@ -4,11 +4,15 @@ from celery import Celery
 from flask import Flask, request, jsonify, render_template
 from minio import Minio
 from werkzeug.utils import secure_filename
+from flask_sqlalchemy import SQLAlchemy
+from shared import Base, VideoModel
 
+# Bucket paths
 TMP_BUCKET = 'temp-uploads'
 
-# Database endpoint
+# Database endpoints
 minio_endpoint = os.getenv('MINIO_ENDPOINT')
+postgres_endpoint = os.getenv('POSTGRES_URL')
 
 # Database user
 access_key = os.getenv('MINIO_ROOT_USER')
@@ -24,16 +28,21 @@ templates_path = os.path.join(os.path.dirname(__file__), 'templates')
 broker_url = f"amqp://{rabbitmq_user}:{rabbitmq_pass}@{rabbitmq_host}:5672"
 result_backend = "redis://redis_backend:6379/0"
 
+# Setup and configure flask: allow uploads up to 500MB
 app = Flask(__name__, template_folder=templates_path)
+app.config['MAX_CONTENT_LENGTH'] = 500 * 1024 * 1024
+app.config['SQLALCHEMY_DATABASE_URI'] = postgres_endpoint
+db = SQLAlchemy(app, model_class=Base)
+
+with app.app_context():
+    db.create_all()
+
 celery_app = Celery('tasks',
                     broker=broker_url,
                     backend=result_backend)
 
-# Allow uploads up to 500MB
-app.config['MAX_CONTENT_LENGTH'] = 500 * 1024 * 1024
-
-# Get connection to database
-storage = Minio(minio_endpoint, access_key, secret_key , secure=False)
+# Get connection to blob storage database
+storage = Minio(minio_endpoint, access_key, secret_key, secure=False)
 
 def get_file_hash(file_stream):
     hasher = hashlib.sha256()
@@ -41,6 +50,7 @@ def get_file_hash(file_stream):
         hasher.update(chunk)
     file_stream.seek(0)
     return hasher.hexdigest()
+
 
 @app.route('/upload', methods=['GET'])
 def upload_page():
@@ -76,9 +86,18 @@ def upload_file():
                        object_name=file_hash,
                        data=file.stream,
                        length=file_size,
-                       part_size=10*1024*1024,
+                       part_size=10 * 1024 * 1024,
                        content_type=file.content_type,
                        metadata=file_metadata)
+
+    # Add video metadata to SQL database
+    # TODO: Link data to user
+    video_data = VideoModel(
+        video_hash=file_hash,
+        file_name=file_name
+    )
+    db.session.add(video_data)
+    db.session.commit()
 
     # Start video transcoding process
     celery_app.send_task('tasks.transcode_video', args=[file_hash])
